cb3e2287aeb1f6606802dc71772f6c30b4c1a2b0
Daniel Leuchtenberg <dleuchtenberg89@gmail.com>
2026-01-22 16:43:18 +0100

    EventEnded: Archivierungs-Hinweis entfernt

--- client/src/components/EventEnded.js
+++ client/src/components/EventEnded.js
@@ -12,9 +12,6 @@ function EventEnded() {
           <p style={{ color: '#666', fontSize: '18px', lineHeight: '1.6' }}>
             Leider ist diese Veranstaltung bereits beendet.
           </p>
-          <p style={{ color: '#999', fontSize: '14px', marginTop: '20px' }}>
-            Die Veranstaltung wurde archiviert und ist nicht mehr verfügbar.
-          </p>
         </div>
       </div>
     </div>
